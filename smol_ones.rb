a96267def5341acb2e083c3d11ba8178aa164973
MikkoBoy <mikko.ahonen666@gmail.com>
2020-11-06 23:11:50 +0200

    Jepa

--- smol_ones.rb
+++ smol_ones.rb
@@ -1,4 +1,7 @@
 # def count_sheep(num)
+  #product vertaa arrayt keskenää, 1..-1 == loputon subarray litanja
+  #inject == enumerable methodi jolla yhistetään kaikki elementit ja lisätään operatori jota käytetään. reduce = injectin alias
+  #ota toi .max pois niin näkee miten se toimii
 #   (1..num).map {|i| "#{i} sheep..."}.join
 # end
 
