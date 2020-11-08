4e1179085bb1c4203a6c5a0d3162821b65db5b37
MikkoBoy <mikko.ahonen666@gmail.com>
2020-11-08 22:27:08 +0200

    Odd int tidy

--- find_odd_int.rb
+++ find_odd_int.rb
@@ -1,7 +1,7 @@
 #My version for 2.7
 def find_it(seq)
-  seq.tally.map {|num, id| 
-    if id.odd?
+  seq.tally.map {|num, len| 
+    if len.odd?
       return num
     end    
     }
@@ -12,8 +12,8 @@ puts find_it([2,2,3,3,4,4,4])
 #Version for codewars, because the Kata was running ruby 2.5 hence no tally method
 # def find_it(seq)
 #   counts = seq.group_by(&:itself)
-#   puts counts.map {|num, id|
-#     if id.length.odd?
+#   puts counts.map {|num, len|
+#     if len.length.odd?
 #       return num
 #     end
 #   }
